fdf26d5aab407c544fda52660100011a2fbae699
Ashwin Upadhyaya <ashwin67@gmail.com>
2019-03-03 08:30:21 +0100

    Better sounds but limited to 10 notes

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Task 1: Get a window working on the raspberry pi where touch and mouse data from the tablet are obtained
 * On a raspberry pi (3+), sudo apt-get install qt5-default pyqt5-dev pyqt5-dev-tools
 * Dependencies such as sip should automatically be installed.
+* pip3 install pyqt5
 * I am using a wacom intuos for this task
 * python3 tablet.py # Python3 is required
 
@@ -14,7 +15,7 @@ Task 2: Create sound changes based on the touch / mouse data
 * python3 soundboard.py
 
 Task 3: Create Chords etc using Fluidsynth
-* pip3 install pyaudio
+* sudo apt-get install fluidsynth
 * Download https://github.com/nwhitehead/pyfluidsynth
 * sudo python3 setup.py install
 * python3 chord.py
--- chordboard.py
+++ chordboard.py
@@ -8,35 +8,59 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
+notes = [
+54, #b1
+55, #c2
+57, #d2
+59, #e2
+60, #f2
+62, #g2
+64, #a2
+66, #b2
+67, #c3
+69  #d3
+]
+notes_mapping = [
+[0,10],
+[11,20],
+[21,30],
+[31,40],
+[41,50],
+[51,60],
+[61,70],
+[71,80],
+[81,90],
+[91,100]
+]
 import pyaudio
-import numpy as np
+#import numpy as np
 
 class TouchWindow(QWidget):
     def __init__(self, parent=None):
+        # Initialize a QT window
         super(TouchWindow, self).__init__(parent)
         self.text = ""
-
         # Resizing the sample window to full desktop size:
         frame_rect = app.desktop().frameGeometry()
         self.max_width, self.max_height = frame_rect.width(), frame_rect.height()
         self.resize(self.max_width, self.max_height)
-        #self.move(-9, 0)
-        self.setWindowTitle("Wacom Draw Area")
         self.width = self.max_width
         self.height = self.max_height
-        
-        self.audio = pyaudio.PyAudio()
-        self.strm = self.audio.open(format = pyaudio.paInt16, channels = 2, rate = 44100, output = True)
-        self.fl = fluidsynth.Synth()
-        self.sfid = self.fl.sfload("/usr/share/sounds/sf2/FluidR3_GM.sf2")
-        self.fl.program_select(0, self.sfid, 0, 100)
-        
-        self.volume = 50
-        self.freq = 50
-        self.old_volume = self.volume
-        self.old_freq = self.freq
+        self.setWindowTitle("Wacom Draw Area")
 
+        # Create Audio
+        self.audio = pyaudio.PyAudio()
         self.sample_rate = 44100
+        self.strm = self.audio.open(format = pyaudio.paInt16, channels = 2, rate = self.sample_rate, output = True)
+        self.fl = fluidsynth.Synth()
+        self.sfid = self.fl.sfload("/usr/share/sounds/sf2/FluidR3_GM.sf2") #TODO: Remove this hard coding later
+        self.fl.program_select(0, self.sfid, 0, 2)
+
+        # Initialize some values
+        self.volume = 50
+        self.n1 = 50
+        self.old_volume = self.volume
+        self.old_n1 = self.n1
         self.samples = []
         self.dur = 0.25
 
@@ -44,30 +68,35 @@ class TouchWindow(QWidget):
         self.text = 'mode: ' + mode + '\n'
         self.text += 'x: ' + str(self.curr_x) + '\n'
         self.text += 'y: ' + str(self.curr_y)
-        self.text += 'Freq: ' + str(self.freq)
+        self.text += 'Freq: ' + str(self.n1)
         self.text += 'Volume: ' + str(self.volume)
         if mode == 'tablet':
             self.text += '\nPressure: ' + str(self.pen_pressure)
 
     def emit_sound(self):
-        self.freq = int(self.curr_x/40)+65
-        self.volume = int((1000-self.curr_y)/9)
-        if abs(self.freq - self.old_freq) > 2:
-            self.fl.noteon(0, self.freq, self.volume)
-            self.fl.noteoff(0, self.old_freq)
-            self.old_freq = self.freq
+        x_rel = int(100*self.curr_x/self.width)
+        self.volume = 0
+        for i in range(len(notes_mapping)):
+            if (x_rel >= notes_mapping[i][0] and x_rel <= notes_mapping[i][1]):
+                self.n1 = notes[i]
+                self.volume = 100
+                break
+
+        if abs(self.n1 - self.old_n1) >= 1:
+            self.fl.noteon(0, self.n1, self.volume)
+            self.fl.noteoff(0, self.old_n1)
+            self.old_n1 = self.n1
             self.old_volume = self.volume
         else:
             pass
-        self.samples = []
-        self.samples = np.append(self.samples, self.fl.get_samples(int(self.sample_rate * self.dur)))
-        
-        #self.fl.noteoff(0, self.freq)
+        self.samples = self.fl.get_samples(int(self.sample_rate * self.dur))
+
+        #self.fl.noteoff(0, self.n1)
         #self.samples = np.append(self.samples, self.fl.get_samples(int(self.sample_rate * self.dur)))
-        
+
         samps = fluidsynth.raw_audio_string(self.samples)
         self.strm.write(samps)
-            
+
     def on_tabletPress(self, tabletEvent):
         print ('Pen Down at:', self.curr_x, self.curr_y)
 
@@ -95,7 +124,7 @@ class TouchWindow(QWidget):
             self.on_tabletMove(tabletEvent)
         elif tabletEvent.type() == QTabletEvent.TabletRelease:
             self.on_tabletRelease(tabletEvent)
-        
+
         self.emit_sound()
         self.display('tablet')
         tabletEvent.accept()
@@ -120,7 +149,7 @@ class TouchWindow(QWidget):
         self.emit_sound()
         mousePressEvent.accept()
         self.update()
-                
+
     def paintEvent(self, event):
         text = self.text
         i = text.find("\n\n")
